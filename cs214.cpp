6314133a00448c76eb431f30e3b656f287026499
imKenda <kadi86195@gmail.com>
2026-05-14 03:09:00 +0300

    added countservedPatients function

--- cs214.cpp
+++ cs214.cpp
@@ -8,3 +8,29 @@ struct Node {
 };
 
 Node* head = NULL;
+
+
+// This function counts served patients
+int countServedPatients() {
+
+    // Start from the first node
+    Node* temp = head;
+
+    // Variable to store the count
+    int count = 0;
+
+    // Traverse the linked list
+    while (temp != NULL) {
+
+        // Check if patient status is "Served"
+        if (temp->status == "Served") {
+            count++;
+        }
+
+        // Move to the next node
+        temp = temp->next;
+    }
+
+    // Return total served patients
+    return count;
+}
